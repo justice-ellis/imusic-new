a27a8bb0fdb9139f580a71012dc8a20c161ec9ef
justice-ellis <justice.ellis@amalitech.org>
2022-10-19 16:13:21 -0800

    make it better

--- config/config.ts
+++ config/config.ts
@@ -8,6 +8,7 @@ dotenv.config({
 export const port = Number(process.env.API_PORT);
 export const db_host = String(process.env.DB_HOST);
 export const db_port = Number(process.env.DB_PORT);
+export const db_url = Number(process.env.DB_URL);
 export const db_name = String(process.env.DB_NAME);
 export const db_user = String(process.env.DB_USER);
 export const db_password = String(process.env.DB_PASSWORD);
--- config/db.ts
+++ config/db.ts
@@ -1,12 +1,14 @@
-import { db_host, db_port, db_name, db_user, db_password } from './config';
-
+import { db_host, db_port, db_name, db_user, db_password, db_url } from './config';
+require('dotenv').config();
 import { Sequelize } from 'sequelize';
 
- export default new Sequelize({
-  dialect: "postgres",
-  host: db_host,
-  port: db_port,
-  database: db_name,
-  username: db_user,
-  password: db_password
-});
+//  export default new Sequelize({
+//   dialect: "postgres",
+//   host: db_host,
+//   port: db_port,
+//   database: db_name,
+//   username: db_user,
+//   password: db_password
+// });
+export default new Sequelize("postgres://tqflfjvofcxxuy:bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502@ec2-44-209-158-64.compute-1.amazonaws.com:5432/dbo1eeou2qo6hs") 
+
