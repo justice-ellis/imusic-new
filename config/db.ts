530fee8bf34093271e09c8462cefe997422891f3
justice-ellis <justice.ellis@amalitech.org>
2022-10-19 17:30:41 -0800

    make it better

--- config/db.ts
+++ config/db.ts
@@ -10,5 +10,5 @@ import { Sequelize } from 'sequelize';
 //   username: db_user,
 //   password: db_password
 // });
-export default new Sequelize("postgres://tqflfjvofcxxuy:bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502@ec2-44-209-158-64.compute-1.amazonaws.com:5432/dbo1eeou2qo6hs") 
+export default new Sequelize("postgres://tqflfjvofcxxuy:bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502@ec2-44-209-158-64.compute-1.amazonaws.com:5432/dbo1eeou2qo6hspostgres://tqflfjvofcxxuy:bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502@ec2-44-209-158-64.compute-1.amazonaws.com:5432/dbo1eeou2qo6hs") 
 
