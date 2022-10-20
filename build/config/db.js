9de3fc55883ae5e90438487c796fbbe2f0e077d7
justice-ellis <justice.ellis@amalitech.org>
2022-10-19 18:05:36 -0800

    make it better

--- build/config/db.js
+++ build/config/db.js
@@ -10,13 +10,13 @@ const sequelize_1 = require("sequelize");
 //   username: db_user,
 //   password: db_password
 // });
-// export default new Sequelize({
-//     dialect: "postgres",
-//     host: "ec2-44-209-158-64.compute-1.amazonaws.com",
-//     port: 5432,
-//     database: "dbo1eeou2qo6hs",
-//     username: "tqflfjvofcxxuy",
-//     password: "bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502"
-//   });
-exports.default = new sequelize_1.Sequelize("postgres://tqflfjvofcxxuy:bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502@ec2-44-209-158-64.compute-1.amazonaws.com:5432/dbo1eeou2qo6hs");
+exports.default = new sequelize_1.Sequelize({
+    dialect: "postgres",
+    host: "ec2-44-209-158-64.compute-1.amazonaws.com",
+    port: 5432,
+    database: "dbo1eeou2qo6hs",
+    username: "tqflfjvofcxxuy",
+    password: "bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502"
+});
+// export default new Sequelize("postgres://tqflfjvofcxxuy:bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502@ec2-44-209-158-64.compute-1.amazonaws.com:5432/dbo1eeou2qo6hs") 
 //# sourceMappingURL=db.js.map
--- config/db.ts
+++ config/db.ts
@@ -11,13 +11,13 @@ import { Sequelize } from 'sequelize';
 //   password: db_password
 // });
 
-// export default new Sequelize({
-//     dialect: "postgres",
-//     host: "ec2-44-209-158-64.compute-1.amazonaws.com",
-//     port: 5432,
-//     database: "dbo1eeou2qo6hs",
-//     username: "tqflfjvofcxxuy",
-//     password: "bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502"
-//   });
-export default new Sequelize("postgres://tqflfjvofcxxuy:bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502@ec2-44-209-158-64.compute-1.amazonaws.com:5432/dbo1eeou2qo6hs") 
+export default new Sequelize({
+    dialect: "postgres",
+    host: "ec2-44-209-158-64.compute-1.amazonaws.com",
+    port: 5432,
+    database: "dbo1eeou2qo6hs",
+    username: "tqflfjvofcxxuy",
+    password: "bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502"
+  });
+// export default new Sequelize("postgres://tqflfjvofcxxuy:bc8cf446cd505e190e1fe100e078f631f40de83ff4ab3aee588be99ae28f4502@ec2-44-209-158-64.compute-1.amazonaws.com:5432/dbo1eeou2qo6hs") 
 
